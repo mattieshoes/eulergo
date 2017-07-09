7694b8aeb2920c17e21f2ded6c7e825c98390d88
Mattie <mattieshoes@gmail.com>
2017-07-08 18:54:07 -0600

    gofmt on files

--- 9.go
+++ 9.go
@@ -1,12 +1,12 @@
 /*
-    A Pythagorean triplet is a set of three natural numbers, a < b < c, for 
-    which,
+   A Pythagorean triplet is a set of three natural numbers, a < b < c, for
+   which,
 
-    a^2 + b^2 = c^2
-    For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+   a^2 + b^2 = c^2
+   For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
-    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-    Find the product abc.
+   There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+   Find the product abc.
 */
 
 package main
@@ -23,7 +23,7 @@ func main() {
 	found := false
 
 	for a = 1; a < 333; a++ {
-		for b = a + 1; 1000 - a - b > b; b++ {
+		for b = a + 1; 1000-a-b > b; b++ {
 			c = 1000 - a - b
 			if a*a+b*b == c*c {
 				found = true
